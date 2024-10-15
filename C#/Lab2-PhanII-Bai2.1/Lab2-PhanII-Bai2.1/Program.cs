0895866f3c65c039e1c447ba43f401c3e5b36dd7
Nguyễn Viết Chiến <chien08032003@gmail.com>
2024-10-15 09:53:09 +0700

    add folder

---
+++ C#/Lab2-PhanII-Bai2.1/Lab2-PhanII-Bai2.1/Program.cs
@@ -0,0 +1,59 @@
+﻿class NhanVien
+{
+    public int MaNV { get; set; }
+    public string Ten { get; set; }
+    public string DiaChi { get; set; }
+    public DateTime NgaySinh { get; set; }
+    public decimal Luong { get; set; }
+    public decimal Thuong { get; set; }
+
+
+    public decimal TongLuong
+    {
+        get
+        {
+            return Luong + Thuong;
+        }
+    }
+
+
+    public void HienThiThongTin()
+    {
+        Console.WriteLine($"Ma NV: {MaNV}");
+        Console.WriteLine($"Ten: {Ten}");
+        Console.WriteLine($"Đia chi: {DiaChi}");
+        Console.WriteLine($"Ngay sinh: {NgaySinh.ToShortDateString()}");
+        Console.WriteLine($"Luong: {Luong}");
+        Console.WriteLine($"Thuong: {Thuong}");
+        Console.WriteLine($"Tong luong: {TongLuong}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        NhanVien nhanVien = new NhanVien();
+
+        Console.Write("Nhap ma nhan vien: ");
+        nhanVien.MaNV = int.Parse(Console.ReadLine());
+
+        Console.Write("Nhap ten nhan vien: ");
+        nhanVien.Ten = Console.ReadLine();
+
+        Console.Write("Nhap đia chi: ");
+        nhanVien.DiaChi = Console.ReadLine();
+
+        Console.Write("Nhap ngay sinh (MM/DD/YYYY): ");
+        nhanVien.NgaySinh = DateTime.Parse(Console.ReadLine());
+
+        Console.Write("Nhap luong: ");
+        nhanVien.Luong = decimal.Parse(Console.ReadLine());
+
+        Console.Write("Nhap thuong: ");
+        nhanVien.Thuong = decimal.Parse(Console.ReadLine());
+
+        Console.WriteLine("\nThong tin nhan vien:");
+        nhanVien.HienThiThongTin();
+    }
+}
---
+++ C#/Lab2-PhanII-Bai2.2/Lab2-PhanII-Bai2.2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Nhap nam: ");
+        int nam = int.Parse(Console.ReadLine());
+
+        Console.Write("Nhap thang: ");
+        int thang = int.Parse(Console.ReadLine());
+
+        int soNgay = DateTime.DaysInMonth(nam, thang);
+
+        Console.WriteLine($"So ngay trong thang {thang} nam {nam} la: {soNgay}");
+    }
+}
+
