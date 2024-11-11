b97b766c1b5c0c908b15f18a8619020f3a6ff95d
Nguyễn Viết Chiến <130949450+Nguyen-Chien-99@users.noreply.github.com>
2024-11-11 19:37:06 +0700

    update

--- Project/Project/obj/Debug/net8.0/Project.AssemblyInfo.cs
+++ Project/Project/obj/Debug/net8.0/Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e55ac31c1ee5dba95a5de7e0a972aa797ce10fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2297cd40cdc48632cf323b767839adaf98d3909")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
