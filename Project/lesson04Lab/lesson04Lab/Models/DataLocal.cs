51b896fbc1e88384e94fd7ad96d39d4eefa9dfc9
Nguyễn Viết Chiến <130949450+Nguyen-Chien-99@users.noreply.github.com>
2024-11-14 10:36:46 +0700

    update

---
+++ Project/lesson04Lab/lesson04Lab/Models/DataLocal.cs
@@ -0,0 +1,81 @@
+﻿using System.Net.NetworkInformation;
+
+namespace lesson04Lab.Models
+{
+    public class DataLocal
+    {
+        public static List<People> _peoples = new List<People>()
+        {
+            new People() {
+                Id=0,
+                Name="Devmaster",
+                Email="Vanan9524@gmail.com",
+                Phone="0327762985",
+                Address="25 vũ ngọc phan",
+                Avatar="/images/avatar/00.jfif",
+                Birthday=Convert.ToDateTime("2003/09/28"),
+                Bio="viện công nghệ devmaster",Gender=0
+            },
+            new People() {
+                Id=1,Name="Devmaster",
+                Email="Vanan9524@gmail.com",
+                Phone="0327762985",
+                Address="25 vũ ngọc phan",
+                Avatar="/images/avatar/01.jfif",
+                Birthday=Convert.ToDateTime("2003/09/28"),
+                Bio="viện công nghệ devmaster",
+                Gender=0
+            },
+             new People() {
+                Id=2,Name="Devmaster",
+                Email="Vanan9524@gmail.com",
+                Phone="0327762985",
+                Address="25 vũ ngọc phan",
+                Avatar="/images/avatar/02.jfif",
+                Birthday=Convert.ToDateTime("2003/09/28"),
+                Bio="viện công nghệ devmaster",
+                Gender=0
+            },
+              new People() {
+                Id=3,Name="Devmaster",
+                Email="Vanan9524@gmail.com",
+                Phone="0327762985",
+                Address="25 vũ ngọc phan",
+                Avatar="/images/avatar/03.jfif",
+                Birthday=Convert.ToDateTime("2003/09/28"),
+                Bio="viện công nghệ devmaster",
+                Gender=0
+            },
+               new People() {
+                Id=4,Name="Devmaster",
+                Email="Vanan9524@gmail.com",
+                Phone="0327762985",
+                Address="25 vũ ngọc phan",
+                Avatar="/images/avatar/04.jfif",
+                Birthday=Convert.ToDateTime("2003/09/28"),
+                Bio="viện công nghệ devmaster",
+                Gender=0
+            },
+                new People() {
+                Id=5,Name="Devmaster",
+                Email="Vanan9524@gmail.com",
+                Phone="0327762985",
+                Address="25 vũ ngọc phan",
+                Avatar="/images/avatar/05.jfif",
+                Birthday=Convert.ToDateTime("2003/09/28"),
+                Bio="viện công nghệ devmaster",
+                Gender=0
+            }
+
+        };
+        public static List <People> GetPeoples()
+        {
+            return _peoples;
+        }
+        public static People ? GetPeopleById(int id)
+        {
+            var people = _peoples.FirstOrDefault(x => x.Id == id);
+            return people;
+        }
+    }
+}
---
+++ Project/lesson04Lab/lesson04Lab/Models/People.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Net.NetworkInformation;
+
+namespace lesson04Lab.Models
+{
+    public class People
+    {
+        public int Id { get; set; }
+        [Display(Name="Họ và tên")]
+        public string Name { get; set; }
+        [Display (Name="Địa chỉ Email")]
+        public string Email { get; set; }
+        [Display(Name="Số điện thoại")]
+        public string Phone { get; set; }
+        [Display (Name="Địa chỉ nơi ở")]
+        public string Address { get; set; }
+        [Display(Name = "Ảnh điện thoại")]
+        public string Avatar { get; set; }
+        [Display(Name = "Ngày sinh")]
+        public DateTime Birthday { get; set; }
+        [Display(Name = "Giới thiệu bản thân")]
+        public string Bio {  get; set; }
+        [Display(Name = "Giới tính")]
+        public byte Gender { get; set; }
+    }
+}
