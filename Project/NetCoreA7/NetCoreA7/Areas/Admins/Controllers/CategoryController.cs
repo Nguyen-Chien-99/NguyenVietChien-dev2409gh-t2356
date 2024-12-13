e7d9539123fe7ebec94361e5096ec6183e162bab
Nguyễn Viết Chiến <130949450+Nguyen-Chien-99@users.noreply.github.com>
2024-12-13 17:15:30 +0700

    update

---
+++ Project/NetCoreA7/NetCoreA7/Areas/Admins/Controllers/CategoryController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NetCoreA7.Areas.Admins.Controllers
+{
+    public class CategoryController : Controller
+    {
+        [Area("Admins")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
